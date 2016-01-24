9fa362b2ba5069fca5724de2303dfff61c5d0d22
cgillies@umich.edu <cgillies@umich.edu>
2016-01-24 11:30:00 -0500

    started enumerator

---
+++ src/main/java/org/kidneyomics/hmm/Enumerator.java
@@ -0,0 +1,14 @@
+package org.kidneyomics.hmm;
+
+public class Enumerator<T> {
+	
+	/*
+	 * TODO: use binary tree to construct all symbol combinations
+	 * 
+	 * input wil be a list of states and each level will correspond to the
+	 * length of enumeration
+	 */
+	public Enumerator<T> getEnumerator() {
+		return new Enumerator<T>();
+	}
+}
